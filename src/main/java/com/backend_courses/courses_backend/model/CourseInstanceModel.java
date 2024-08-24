369f8dd7882966c9ea4ae89a10be61b518313469
tanmay-S12 <tanmaysankpal007@gmail.com>
2024-08-24 21:20:26 +0530

    Course and CourseEntity models added

---
+++ src/main/java/com/backend_courses/courses_backend/model/CourseInstanceModel.java
@@ -0,0 +1,33 @@
+package com.backend_courses.courses_backend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@NoArgsConstructor 
+@AllArgsConstructor
+public class CourseInstanceModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; 
+    
+    @Column(nullable = false)
+    private int year; 
+    
+    @Column(nullable = false)
+    private int semester; 
+
+     @ManyToOne 
+     @JoinColumn(name = "course_id", nullable = false) 
+     private CourseModel course; 
+    }
+
---
+++ src/main/java/com/backend_courses/courses_backend/model/CourseModel.java
@@ -0,0 +1,26 @@
+package com.backend_courses.courses_backend.model;
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Data 
+@NoArgsConstructor 
+@AllArgsConstructor
+public class CourseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false) 
+    private String title;
+
+    @Column(nullable = false, unique = true) 
+    private String courseCode;
+
+    @Column(length = 1000) 
+    private String description;
+}
+
